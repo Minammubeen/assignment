3052c0a65b77440401e147a834f33d7e4719ecaf
ibaaddd <ibaad_ali@hotmail.com>
2021-12-15 21:11:01 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,50 @@
+var firstName = prompt("Enter Your First Name");
+var lastName  = prompt("Enter Your Last Name");
+var fullName = (firstName + lastName);
+console.log(fullName);
+
+var str = prompt("Enter Your Favourite Mobile Phone Model");
+var numChars = str.length;
+ 
+var arr = ("Pakistani");
+console.log(arr[7]);
+
+var arr = ("Hello World");
+console.log(arr[9]);
+
+var arr = ("Pakistani");
+console.log(arr[3]);
+
+var firstName = "Minam";
+var lastName = "Mubeen";
+var fullName = (firstName + lastName);
+console.log(fullName);
+
+var arr = ["HYDERABAD"];
+arr[0] = "ISLAMABAD";
+console.log(arr);
+
+var arr = ["Ali and Sami Are Best Friends. They Play cricket And football together."]
+var newArray = arr.splice(3,3);
+console.log(newArray);
+
+area = S(S − a)(S − b)(S − c)
+where, S = ( a + b + c ) / 2
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
